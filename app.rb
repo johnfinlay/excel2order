4a5b8bda3e3b449606041fac50108f00dab83e06
John Finlay <finlay_john@hotmail.com>
2020-10-02 09:49:53 -0500

    Add logging to SQL database

--- app.rb
+++ app.rb
@@ -38,10 +38,42 @@ require 'mail'
 require 'creek'
 require 'tiny_tds'
 
+SQL_DEFAULTS = ['SET ANSI_DEFAULTS ON',
+                'SET QUOTED_IDENTIFIER ON',
+                'SET CURSOR_CLOSE_ON_COMMIT OFF',
+                'SET IMPLICIT_TRANSACTIONS OFF',
+                'SET TEXTSIZE 2147483647',
+                'SET CONCAT_NULL_YIELDS_NULL ON',
+                'USE [P21]']
+
 # Connect to SQL Server
-@client = TinyTds::Client.new username: 'admin', password: 'Nel$0n!($&', host: 'SQL1', port: 1433
-puts 'Connecting to SQL Server'
+def sql_default_settings(client)
+  puts 'Setting SQL defaults...'
 
-puts 'Done' if @client.active?
+  SQL_DEFAULTS.each do |sql|
+    result = client.execute(sql)
+    result.do
+    result.cancel
+  end
 
-@client.close
+  puts 'Done'
+end
+
+def log_to_sql(log_type, log_description)
+  client = TinyTds::Client.new username: 'admin',
+                           password: 'Nel$0n!($&',
+                           host: 'SQL1', port: 1433
+  puts 'Connecting to SQL Server...'
+  puts 'Done' if client.active?
+  sql_default_settings(client)
+  
+  sql = "INSERT INTO [nws_log_emailed_orders] ([date_created], [log_type], "\
+        "[log_description]) VALUES (CAST('#{DateTime.now}' AS DATETIME2), "\
+        "'#{client.escape(log_type)}', '#{client.escape(log_description)}')"
+  puts sql
+  result = client.execute(sql)
+  p result.insert
+  puts "#{result.affected_rows} row(s) affected" # if result.affected_rows > 0
+  client.close
+end
+
