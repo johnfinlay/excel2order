81c9650c5b3484a98186030ae29e4f16ad76dd4c
John Finlay <finlay_john@hotmail.com>
2020-10-02 09:56:04 -0500

    cleanup puts statements

--- app.rb
+++ app.rb
@@ -15,7 +15,6 @@
     Output - Tab delimited order import files
     YAML file for configuration
     Log files for errors and previous emails/attachments processed
-      * SQL table???
 
   Algorithm
     get_email method
@@ -46,7 +45,6 @@ SQL_DEFAULTS = ['SET ANSI_DEFAULTS ON',
                 'SET CONCAT_NULL_YIELDS_NULL ON',
                 'USE [P21]']
 
-# Connect to SQL Server
 def sql_default_settings(client)
   puts 'Setting SQL defaults...'
 
@@ -70,10 +68,12 @@ def log_to_sql(log_type, log_description)
   sql = "INSERT INTO [nws_log_emailed_orders] ([date_created], [log_type], "\
         "[log_description]) VALUES (CAST('#{DateTime.now}' AS DATETIME2), "\
         "'#{client.escape(log_type)}', '#{client.escape(log_description)}')"
-  puts sql
   result = client.execute(sql)
-  p result.insert
+  result.insert
   puts "#{result.affected_rows} row(s) affected" # if result.affected_rows > 0
   client.close
 end
 
+def get_email()
+
+end
